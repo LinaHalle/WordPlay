7a05a16f7d11a2f2f514bbd7d5a9dfb53225528f
Piexgear <davidpuscas@live.se>
2026-04-14 10:36:23 +0200

    added the dotnet run part but needs test

--- WordPlay/playwright.config.ts
+++ WordPlay/playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig} from '@playwright/test';
 
 
 /**
@@ -12,55 +12,18 @@ import { defineConfig, devices } from '@playwright/test';
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-export default defineConfig({
-  webServer: {
-    command: "npm run start",
-    url: "http://localhost:5173",
-    reuseExistingServer: !process.env.CI,
-  },
-
-  /* Configure projects for major browsers */
-  projects: [
+export default defineConfig(
+  {
+    webServer: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      command: "npm run start",
+      url: "http://localhost:5173",
+      reuseExistingServer: !process.env.CI,
     },
-
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+      command: "dotnet run",
+      url: "http://localhost:5000",
+      reuseExistingServer: !process.env.CI,
+    }
+  ]
 });
