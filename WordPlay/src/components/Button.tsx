c352f9f349a7ea7691747743409b99f778704a43
Jolle-Jay <j.lopezzz@hotmail.com>
2026-04-16 15:31:17 +0200

    disable

--- WordPlay/src/components/Button.tsx
+++ WordPlay/src/components/Button.tsx
@@ -2,11 +2,13 @@ type Props = {
   children: React.ReactNode;
   className?: string;
   onClick?: () => void;
+  disabled?: boolean;
 };
 
-export default function Button({ children, className = "", onClick }: Props) {
+export default function Button({ children, className = "", onClick, disabled }: Props) {
   return (
-    <button className={`btn ${className}`} onClick={onClick}>{children}
+    <button className={`btn ${className}`} onClick={onClick} disabled={disabled}>
+      {children}
     </button>
   );
 }
