5d7068564bb90fa91b23ec634843d12b2a1d93c8
linahalle <hallergren.lina@gmail.com>
2026-04-16 13:43:27 +0200

    dunno

--- WordPlay/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ WordPlay/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff9dab70e21435e8a18a5c9bf4944164470099bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5ea5c56bfe91c2ea14d4d761035516393e5715b")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
