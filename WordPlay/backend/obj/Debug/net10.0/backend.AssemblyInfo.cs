544383a4a1f2fabf52e0ef6d2f03f7cb4e6857dd
linahalle <hallergren.lina@gmail.com>
2026-04-16 13:42:35 +0200

    did a git pull

--- WordPlay/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ WordPlay/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87fd279c2e6a49a375ce3b50acfb8e78aef0704c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8cf25f6a0b59310ecbaf81acff0d7c6133be7d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
