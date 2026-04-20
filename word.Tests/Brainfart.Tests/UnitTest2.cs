0dd4ae2bcb702750294a1e54c1b15ca5e5470c35
Jolle-Jay <j.lopezzz@hotmail.com>
2026-04-20 10:49:37 +0200

    making scoring xunit test

--- word.Tests/Brainfart.Tests/UnitTest2.cs
+++ word.Tests/Brainfart.Tests/UnitTest2.cs
@@ -1,4 +1,15 @@
 namespace Brainfart.Tests;
 
-public class
+public class CreateGameTest
+{
+  [Fact]
+  public void CreateGameTest()
+  {
+    GameService service = new GameService();
+    var hostName = "Alice";
+
+    var(gameId, playerId, error)
+  }
+}
+
 
